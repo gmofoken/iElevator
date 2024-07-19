e1ad9408747a154709d93dc9c22a51d2c0c3e7ea
Gomotso Mofokeng <Gomotso.Mofokeng@psg.co.za>
2024-07-19 02:25:39 +0200

    Added ControlSystem Project and UnitTest Project

---
+++ ElevatorControl/ElevatorControlSystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElevatorControlSystem
+{
+    public interface IElevatorControl
+    {
+        void RegisterObserver(int elevatorId, IButtonPressObserver observer);
+
+        void UnregisterObserver(int elevatorId, IButtonPressObserver observer);
+
+        void ButtonPressed(int elevatorId, int floor);
+    }
+
+    public class ElevatorControl : IElevatorControl
+    {
+        private Dictionary<int, List<IButtonPressObserver>> elevatorObservers = new Dictionary<int, List<IButtonPressObserver>>();
+
+        public void RegisterObserver(int elevatorId, IButtonPressObserver observer)
+        {
+            if (!elevatorObservers.ContainsKey(elevatorId))
+                elevatorObservers[elevatorId] = new List<IButtonPressObserver>();
+
+            elevatorObservers[elevatorId].Add(observer);
+        }
+
+        public void UnregisterObserver(int elevatorId, IButtonPressObserver observer)
+        {
+            if (elevatorObservers.ContainsKey(elevatorId))
+                elevatorObservers[elevatorId].Remove(observer);
+        }
+
+        public void ButtonPressed(int elevatorId, int floor)
+        {
+            Console.WriteLine($"Elevator {elevatorId} called to floor {floor}");
+            NotifyObservers(elevatorId, floor);
+        }
+
+        private void NotifyObservers(int elevatorId, int floor)
+        {
+            if (elevatorObservers.ContainsKey(elevatorId))
+            {
+                foreach (var observer in elevatorObservers[elevatorId])
+                {
+                    observer.HandleButtonPress(floor);
+                }
+            }
+        }
+    }
+
+    public interface IButtonPressObserver
+    {
+        void HandleButtonPress(int floor);
+    }
+
+    public class ElevatorDisplay : IButtonPressObserver
+    {
+        public void HandleButtonPress(int floor)
+        {
+            Console.WriteLine($"Elevator Display: Going to floor {floor}");
+        }
+    }
+}
--- iElevator/Program.cs
+++ iElevator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevatorControlSystem;
+using System;
 
 namespace iElevator
 {
@@ -6,7 +7,25 @@ namespace iElevator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var elevatorControl = new ElevatorControl();
+            var elevator1Display = new ElevatorDisplay();
+            var elevator2Display = new ElevatorDisplay();
+
+            // Register observers for different elevators
+            elevatorControl.RegisterObserver(1, elevator1Display);
+            elevatorControl.RegisterObserver(2, elevator2Display);
+
+            // Simulate button presses
+            elevatorControl.ButtonPressed(1, 3);
+            elevatorControl.ButtonPressed(2, 5);
+
+            // Unregister observer for elevator 1
+            //elevatorControl.UnregisterObserver(1, elevator1Display);
+
+            // No notification for elevator 1 after unregistering
+            elevatorControl.ButtonPressed(1, 4);
+            
         }
     }
 }
